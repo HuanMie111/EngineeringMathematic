febfaef6a3f3b886acb6ef66c99be5ec4a5089f7
LAPTOP-L3I3UNEL\girlfriend <1912093464@qq.com>
2021-07-05 20:25:38 +0800

    Gauss-Seidel迭代法

---
+++ Chapter 6 system of linear equations/Gauss-Seidel iterative method.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+while True:
+    print("请输入系数矩阵：")
+    print("示例：[[1.0,2,0],[3.0,4.0]]")
+    A = np.array(eval(input()),dtype = np.float64)  # 系数矩阵 M
+    D = np.linalg.det(A)  # M 行列式
+    [m,n] = np.shape(A)
+    x_last = np.zeros(n)  # 上一步解向量
+    x_next = np.ones(n)  # 下一步解向量
+    if D == 0:
+        print("系数矩阵不满秩，请重新输入")
+        continue
+    else:
+        print("请输入右端项：")
+        print("示例：[1.0,2.0]")
+        b = np.array(eval(input()),dtype = np.float64)  # 右端项 b
+        # 精度计算使用向量的 2-范数
+        epsilon = eval(input("请输入精度（示例：0.0001）："))
+        k = 0  # 迭代次数
+        ActualError = epsilon+1  # 真实误差赋初值  
+        while ActualError > epsilon:
+            for i in range(n):
+                x_com = np.r_[x_next[:i],x_last[i:]]  # 组合上一步和下一部解向量
+                x_next[i] = x_last[i] + (-np.dot(x_com,A[i,:])+b[i])/A[i,i]
+            ActualError = np.linalg.norm(x_next-x_last)
+            x_last = x_next.copy()
+            k = k+1
+        print('解为：',x_next)
+        print("迭代次数：",k)
